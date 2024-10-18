ceb6c285b8ee9f323917f36afcfeb1331a8d1a49
FateUBW0227 <FateUBW@mail.ustc.edu.cn>
2024-10-18 15:37:42 +0800

    add

--- Fiji-BVdata/script.py
+++ Fiji-BVdata/script.py
@@ -5,12 +5,21 @@ from BVMoudle import BVReader
 import scyjava as sj
 
 # initiate ImageJ
-ij = imagej.init(r'E:\reading\BV_project\fiji-win64-pure\Fiji.app', mode='interactive')
+# fiji_path = r'E:\reading\BV_project\fiji-win64-pure\Fiji.app'
+fiji_path = ''
+BV_config_path = 'E:/ZHM_RES/sumplementary/zhm_res/CropImg_BV/config.cfg'
+level = 0
+min_x = 10136
+max_x = min_x + 512
+min_y = 8591
+max_y = min_y + 512
+min_z = 831
+max_z = min_z + 512
 
-def process_image(image_path):
+ij = imagej.init(fiji_path, mode='interactive')
+def process_image():
     reader = BVReader()
-    img = reader.loadROI("E:/ZHM_RES/sumplementary/zhm_res/CropImg_BV/config.cfg", 0, 10136, 10136 + 512, 8591, 8591 + 512, 831, 831 + 512)
-    #img = reader.loadROI("E:/ZHM_RES/sumplementary/zhm_res/CropImg_BV/config.cfg", 3, 0, 12288, 0, 10240, 0, 2048)
+    img = reader.loadROI(BV_config_path, level, min_x, max_x, min_y, max_y, min_z, max_z)
     expand_img = np.expand_dims(img, axis=2).transpose(0, 2, 1, 3)
     processed_img = ij.py.to_imageplus(expand_img)
     while True:
@@ -19,6 +28,6 @@ def process_image(image_path):
         except KeyboardInterrupt:
             break
 
-# 运行图像处理
-image_path = r'E:\reading\BV_project\FIG4\1.png'
-process_image(image_path)
+
+# load images:
+process_image()
